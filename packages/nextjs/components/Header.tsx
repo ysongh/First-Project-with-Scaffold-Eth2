5cde7304dce773e4ba857c9e3690ff39f0023be6
You Song Hou <ysongweb3@gmail.com>
2023-04-08 20:08:36 -0400

    refactor: change network

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Bars3Icon, BugAntIcon, ViewColumnsIcon, WalletIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, ViewColumnsIcon, WalletIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -51,12 +51,12 @@ export const Header = () => {
           My NFT
         </NavLink>
       </li>
-      <li>
+      {/* <li>
         <NavLink href="/debug">
           <BugAntIcon className="h-4 w-4" />
           Debug Contracts
         </NavLink>
-      </li>
+      </li> */}
     </>
   );
 
